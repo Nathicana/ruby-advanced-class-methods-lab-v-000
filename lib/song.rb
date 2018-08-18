eab527ba3de17ff7c8fd718ea363d539c1c1b565
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-18 22:33:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 
   def initialize(name)
   @name = name
-  @@songs << self
+  self.class.all << self
   end
 
   def self.all #class reader method that reads the data stored in class variable @@all#
