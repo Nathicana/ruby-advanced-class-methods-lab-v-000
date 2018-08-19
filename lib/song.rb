02e739fa5cfc0c14a6c2283b03a9486372486ab9
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-19 00:49:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,11 @@ def self.find_by_name(name)
 self.all.find{|song| song.name == name}
 end
 
+def self.find_or_create_by_name 
+end
+
+
+
 def self.alphabetical
     @@all.sort_by {|song| song.sort}
   end
