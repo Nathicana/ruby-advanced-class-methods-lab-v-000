19b4dfa0439dbb47ea2249e39b3696b042416d1f
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-19 05:19:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ def self.alphabetical
   def self.new_from_filename(filename)
     file = filename.split(/([-])/)
     artist_name = file[0].strip
-    name = file[1].strip.gsub
+    name = file[1].strip.gsub(".mp3")
 
 
 
