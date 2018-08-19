cb2b8da81756b6298317f83f301d518901ae1584
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-19 03:56:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,12 +51,12 @@ def self.alphabetical
 
   def self.new_from_filename(filename)
     file = filename.split("-")
-   songname = file[0]
+   name = file[0]
    artist_name = file[1]
 
    new_song = self.new
    new_song.artist_name = artist_name
-   new_song.songname = songname
+   new_song.name = name
    new_song
  end
 
