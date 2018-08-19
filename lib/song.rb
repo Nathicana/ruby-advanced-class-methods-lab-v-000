6ff01e188c09e58a7f8b946495bdfd25e269ac4b
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-19 00:52:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,10 @@ def self.find_by_name(name)
 self.all.find{|song| song.name == name}
 end
 
-def self.find_or_create_by_name 
+def self.find_or_create_by_name(name)
+
+  song.create_by_name(name) unless 
+  song.find_by_name(name) == true
 end
 
 
