7db75e1a6a04c553b266cdb7b2e446be65e99f51
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-19 00:26:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,11 +24,11 @@ def self.new_by_name(name)
     end
 
 
-def self.create_by_name(name)
+def self.create_by_name
   song = self.new
   song.name = name
   @@all << song
-  
+
   end
 
 def self.find_by_name(name)
